991d5d02c4cd10054f3b6437584da68eb6fcf2a4
inoyna11 <82111047+inoyna11@users.noreply.github.com>
2021-05-07 04:43:55 +0800

    Update jd_health.js

--- jd_health.js
+++ jd_health.js
@@ -249,7 +249,7 @@ function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
     $.get({
-      url: `https://cdn.jsdelivr.net/gh/wuzhi-docker1/RandomShareCode@main/JD_Health.json`,headers:{
+      url: `https://raw.githubusercontent.com/inoyna11/jd28/master/backUp/total/jd_health.json`,headers:{
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       },'timeout': 10000
     }, (err, resp, data) => {
