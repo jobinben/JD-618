05fb6a4eb4bb5087e4e9830ee7c4007e9af7bcbd
inoyna11 <82111047+inoyna11@users.noreply.github.com>
2021-05-21 03:04:20 +0800

    Update jd_bean_home.js

--- jd_bean_home.js
+++ jd_bean_home.js
@@ -174,7 +174,7 @@ function doTask2() {
 
 function getAuthorShareCode() {
   return new Promise(resolve => {
-    $.get({url: "https://cdn.jsdelivr.net/gh/wuzhi-docker1/updateTeam/master/shareCodes/jd_updateBeanHome.json",headers:{
+    $.get({url: "https://raw.githubusercontent.com/inoyna11/jd28/master/shareCodes/jd_updateBeanHome.json",headers:{
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
       }}, async (err, resp, data) => {
       try {
