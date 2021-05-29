e4778067374b88bb95abe6e0cc0721e67d239aec
inoyna11 <82111047+inoyna11@users.noreply.github.com>
2021-05-30 01:10:29 +0800

    Update jd_sgmh.js

--- jd_sgmh.js
+++ jd_sgmh.js
@@ -29,7 +29,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let appId = '1EFRXxg' , homeDataFunPrefix = 'interact_template', collectScoreFunPrefix = 'harmony', message = ''
 let lotteryResultFunPrefix = homeDataFunPrefix, browseTime = 6
 const inviteCodes = [
-  'T0225KkcRxgY_VbTIx-llf8KJwCjVWmIaW5kRrbA',
+  'T0225KkcRxgY_VbTIx-llf8KJwCjVQmoaT5kRrbA',
 ];
 const randomCount = 0 ;
 const notify = $.isNode() ? require('./sendNotify') : '';
