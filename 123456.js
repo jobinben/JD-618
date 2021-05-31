ec964b5c79e519f99364419546521f6bfba334ea
inoyna11 <82111047+inoyna11@users.noreply.github.com>
2021-05-31 23:28:42 +0800

    Update 123456.js

--- 123456.js
+++ 123456.js
@@ -565,7 +565,7 @@ async function dealReturn(type, data) {
         console.log(JSON.stringify(data));
       }
       if(data.code === 0 && data.data && data.data.bizCode === -1002){
-        $.hotFlag = true;
+        $.hotFlag = false;
         console.log(`该账户脚本执行任务火爆，暂停执行任务，请手动做任务或者等待解决脚本火爆问题`)
       }
       break;
