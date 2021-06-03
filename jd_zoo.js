b07c598a615be8fff33c184b7c62b4fabbf157c2
inoyna11 <82111047+inoyna11@users.noreply.github.com>
2021-06-03 12:38:08 +0800

    Update jd_zoo.js

--- jd_zoo.js
+++ jd_zoo.js
@@ -109,7 +109,7 @@ if ($.isNode()) {
     $.index = i + 1;
     //console.log($.inviteList);
     //pk助力
-    if (new Date().getHours() >= 9) {
+    if (new Date().getUTCHours() + 8 >= 9) {
       console.log(`\n******开始内部京东账号【怪兽大作战pk】助力*********\n`);
       for (let i = 0; i < $.pkInviteList.length && pKHelpFlag && $.canHelp; i++) {
         console.log(`${$.UserName} 去助力PK码 ${$.pkInviteList[i]}`);
